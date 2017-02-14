2dca589281b438cc58b9083482519efe2fd4cb8a
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-14 14:34:33 -0500

    added more directories and files

---
+++ db/pokemon.rb
@@ -0,0 +1,10 @@
+pokemons = [{
+    "name" : "Bulbasaur",
+    "pok_type" : "Grass",
+    "tutor" : "grass pledge"
+  }, {
+    "name" : "Ivysaur",
+    "pok_type" : "Poison",
+    "tutor" : "frenzy plant"
+  }
+]
