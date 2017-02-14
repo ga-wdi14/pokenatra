d35015cadb6ba19d5495ab7023504b2b537c5c4e
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-14 17:44:40 -0500

    added some methods

--- app.rb
+++ app.rb
@@ -14,3 +14,42 @@ get '/pokemons' do
   @pokemons = Pokemon.all
   erb :"pokemons/index"
 end
+####################
+
+get '/pokemons/new' do
+  erb :"pokemons/new"
+end
+
+####################
+
+get '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/show"
+end
+
+####################
+
+post '/pokemons' do
+  @pokemon = Pokemon.create(name: params[:name], cp: params[:cp], poke_type: params[:poke_type], tutor: params[:tutor], img_url: params[:img_url])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+####################
+
+get "/pokemons/:id/edit" do
+  @pokemon = Pokemon.find(params[:id])
+  erb(:"pokemons/edit")
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemons/#{@pokemon.id}")
+end
+
+####################
+delete '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect("/pokemons")
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ require_relative '../models/pokemon'
 require_relative './pokemon'
 
 Pokemon.destroy_all
-Pokemon.create(name: "Charmander", cp: rand(800), poke_type: "fire", img_url: "https://img.pokemondb.net/artwork/charmander.jpg")
-Pokemon.create(name: "Squirtle", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/squirtle.jpg")
-Pokemon.create(name: "Pikachu", cp: rand(800), poke_type: "lightning", img_url: "https://img.pokemondb.net/artwork/pikachu.jpg")
-Pokemon.create(name: "Bulbasaur", cp: rand(800), poke_type: "grass", img_url: "https://img.pokemondb.net/artwork/bulbasaur.jpg")
-Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", img_url: "https://img.pokemondb.net/artwork/clefairy.jpg")
-Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg")
-Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", img_url: "https://img.pokemondb.net/artwork/gengar.jpg")
-Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
+Pokemon.create(name: "Charmander", cp: rand(800), poke_type: "fire", tutor: "frenzy plant", img_url: "https://img.pokemondb.net/artwork/charmander.jpg")
+Pokemon.create(name: "Squirtle", cp: rand(800), poke_type: "water", tutor: "frenzy plant", img_url: "https://img.pokemondb.net/artwork/squirtle.jpg")
+Pokemon.create(name: "Pikachu", cp: rand(800), poke_type: "lightning", tutor: "frenzy plant", img_url: "https://img.pokemondb.net/artwork/pikachu.jpg")
+Pokemon.create(name: "Bulbasaur", cp: rand(800), poke_type: "grass", tutor: "grass pledge", img_url: "https://img.pokemondb.net/artwork/bulbasaur.jpg")
+Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", tutor: "grass pledge", img_url: "https://img.pokemondb.net/artwork/clefairy.jpg")
+Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", tutor: "grass pledge", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg")
+Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", tutor: "fire pledge", img_url: "https://img.pokemondb.net/artwork/gengar.jpg")
+Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", tutor: "fire pledge", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
