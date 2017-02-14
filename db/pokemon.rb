fcb11072f03fe955dfadbf0aae23cca7bcd426f1
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-14 15:00:04 -0500

    seeded the database

--- db/pokemon.rb
+++ db/pokemon.rb
@@ -1,10 +1,16 @@
-pokemons = [{
-    "name" : "Bulbasaur",
-    "pok_type" : "Grass",
-    "tutor" : "grass pledge"
-  }, {
-    "name" : "Ivysaur",
-    "pok_type" : "Poison",
-    "tutor" : "frenzy plant"
-  }
-]
+# pokemons = [{
+#     "name" : "Bulbasaur",
+#     "pok_type" : "Grass",
+#     "tutor" : "grass pledge"
+#   }, {
+#     "name" : "Ivysaur",
+#     "pok_type" : "Fire",
+#     "tutor" : "frenzy plant"
+#   }, {
+#     "name" : "Venusaur",
+#     "pok_type" : "Poison",
+#     "tutor" : "fire pledge"
+#   }
+# ]
+#
+# Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
